bda6ad941b129f7deafc029fbb4299ff546cbd1a
bladeyeight <samueljperry1991@gmail.com>
2021-11-09 15:57:58 -0500

    too many bugs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_09_190828) do
+ActiveRecord::Schema.define(version: 2021_11_09_182416) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,16 +22,14 @@ ActiveRecord::Schema.define(version: 2021_11_09_190828) do
     t.string "month", null: false
     t.string "year", null: false
     t.string "budget_type", null: false
-    t.bigint "user_id", null: false
-    t.index ["user_id"], name: "index_budgets_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
+    t.integer "age"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "budgets", "users"
 end
